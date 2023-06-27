e5876cf6034093555c4746078f6468eb9888dca1
peach <amoralyse21@gmail.com>
2023-06-28 00:55:39 +0800

    update :)

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,5 +1,7 @@
+/* eslint-disable react/jsx-no-undef */
 import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
+import Sidebar from "./Sidebar";
 
 const Feed = () => {
   return (
@@ -10,7 +12,17 @@ const Feed = () => {
           borderRight: "1px solid #3d3d3d",
           px: { sx: 0, md: 2 },
         }}
-      ></Box>
+      >
+        <Sidebar />
+
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          Copyright 2022 JSM Media
+        </Typography>
+      </Box>
     </Stack>
   );
 };
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,36 @@
+import { Stack } from "@mui/material";
+import { categories } from "../utils/constants";
+
+const selectedCategory = "New";
+const Sidebar = () => (
+  <Stack
+    direction="column"
+    sx={{
+      overflowY: "auto",
+      height: { sx: "auto", md: "95%" },
+      flexDirection: { md: "colummn" },
+    }}
+  >
+    {categories.map((category) => (
+      <button
+        className="category-btn"
+        style={{
+          background: category.name === selectedCategory && "FC1503",
+          color: "white",
+        }}
+        key={category.name}
+      >
+        <span
+          style={{
+            color: category.name === selectedCategory ? "white" : "red",
+            marginRight: "15px",
+          }}
+        >
+          {category.icon}
+        </span>
+        <span>{category.name}</span>
+      </button>
+    ))}
+  </Stack>
+);
+export default Sidebar;
