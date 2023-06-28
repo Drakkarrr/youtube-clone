64b542d145129fedcd3f8748a63d92e3f1a59bce
peach <amoralyse21@gmail.com>
2023-06-29 01:14:54 +0800

    wa nako kasabot :)

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
-const BASE_URL = "https://youtube-v31.p.rapidapi.com/search";
+export const BASE_URL = "https://youtube-v31.p.rapidapi.com";
 
 const options = {
-  url: BASE_URL,
   params: {
-    maxResults: "50",
+    maxResults: 50,
   },
   headers: {
     "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
