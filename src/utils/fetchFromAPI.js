ce3d8e4982ddb8c50ee1560bd71bb043e648fa02
peach <amoralyse21@gmail.com>
2023-06-30 00:00:38 +0800

    wa nako kasabot

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,17 +1,17 @@
 import axios from "axios";
 
-export const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+export const BASE_URL = "https://youtube-v31.p.rapidapi.com/captions";
 
 const options = {
+  url: BASE_URL,
   params: {
-    maxResults: 50,
+    maxResults: "50",
   },
   headers: {
-    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_URL_KEY,
     "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
   },
 };
-
 export const fetchFromAPI = async (url) => {
   const { data } = await axios.get(`${BASE_URL}/${url}`, options);
 
