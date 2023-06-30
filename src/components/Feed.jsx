1771e78c9e28630e2bef819ffdd0b3281509eabe
Drakkarrr <junrey404@gmail.com>
2023-06-30 11:07:34 +0800

    chris tete

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Box, Stack, Typography } from "@mui/material";
 
 import { fetchFromAPI } from "../utils/fetchFromAPI";
-import { Videos, Sidebar } from "./";
+import { Videos, Sidebar } from "../components";
 
 const Feed = () => {
   const [selectedCategory, setSelectedCategory] = useState("New");
--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,17 +1,36 @@
-import axios from "axios";
+// import axios from "axios";
 
-export const BASE_URL = "https://youtube-v31.p.rapidapi.com/captions";
+// export const BASE_URL = "https://youtube-v31.p.rapidapi.com/captions";
+
+// const options = {
+//   url: BASE_URL,
+//   params: {
+//     maxResults: "50",
+//   },
+//   headers: {
+//     "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+//     "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+//   },
+// };
+// export const fetchFromAPI = async (url) => {
+//   const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+//   return data;
+// };
+
+import axios from "axios";
+export const BASE_URL = "https://youtube-v31.p.rapidapi.com";
 
 const options = {
-  url: BASE_URL,
   params: {
-    maxResults: "50",
+    maxResults: 50,
   },
   headers: {
-    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_URL_KEY,
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
     "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
   },
 };
+
 export const fetchFromAPI = async (url) => {
   const { data } = await axios.get(`${BASE_URL}/${url}`, options);
 
